e7662e6bf47d0a9848f40cc25693a96a8b93a2fe
신혜원 <hwkong7_@naver.com>
2025-11-10 16:10:45 +0900

    Initial Commit

---
+++ S25W10MvvmSwiftUi/S25W10MvvmSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W10MvvmSwiftUiApp.swift
+//  S25W10MvvmSwiftUi
+//
+//  Created by Student on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W10MvvmSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ S25W10MvvmSwiftUiTests/S25W10MvvmSwiftUiTests.swift
@@ -0,0 +1,17 @@
+//
+//  S25W10MvvmSwiftUiTests.swift
+//  S25W10MvvmSwiftUiTests
+//
+//  Created by Student on 11/10/25.
+//
+
+import Testing
+@testable import S25W10MvvmSwiftUi
+
+struct S25W10MvvmSwiftUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
